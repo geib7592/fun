5d008cdc7ff6c6fd60b9af2e3456c9890fc2766a
geib7592 <gpeterson@psiquantum.com>
2022-07-14 20:46:45 -0700

    trying to solve all 50

--- fun/sudoku/sudoku.py
+++ fun/sudoku/sudoku.py
@@ -1,21 +1,40 @@
 from collections import Counter
 import os
+from itertools import islice
+
+VALUES = set(range(1, 10))
 
 
 class SudokuBoard:
-    def __init__(self, debug=False):
+    def __init__(self, state=None, debug=False, show_state=True):
         self.debug = debug
+        self.show_state = show_state
         self.update_counter = 0
+        self.p_state = self._get_initial_possible_state()
+        self.initial_state = state
 
-        self.load_test_board()
+        if state is not None:
+            self._update_p_state_from_state(state)
+            assert self.isValid, "not a valid state"
+        else:
+            self._update_p_state_from_state(self.load_test_board())
+
         self.print_state()
 
-        self.p_state = self._get_initial_possible_state()
+    def add_hint(self):
+        idx, value = self.reduce_possible_state()
+        if value != 0:
+            self.update_state(idx, value)
+        else:
+            "uh oh"
 
-        self._update_p_state_from_state()
-        # self.iteratively_solve()
-
-        self.reduce_possible_state()
+    def solve(self):
+        while not self.isSolved():
+            self.add_hint()
+        if self.isSolved() and self.isValid():
+            return 0
+        else:
+            return -1
 
     def load_test_board(self):
         s = (
@@ -29,66 +48,66 @@ class SudokuBoard:
             "800203009"
             "005010300"
         )
-        self.state = list(map(int, s))
-        assert self.isValid()
+        return list(map(int, s))
 
     def _get_initial_possible_state(self):
-        pstate = set(range(1, 10))
-        pstate = [pstate.copy() for i in range(9 * 9)]
+        pstate = [VALUES.copy() for i in range(9 * 9)]
         return pstate
 
     def isValid(self):
         # check for values outside 0-9 integers
-        c = Counter(self.state)
-        if len(set(c.keys()) - set(range(10))) > 0:
+        s = set(self.generate_simple_state())
+        if len(s - set(range(10))) > 0:
             return False
 
-        # now check for duplicates within rows, cols, & squares
-        for element in self._generate_board_elements():
-            if self._contains_illegal_repeats(element):
+        # check for duplicates within rows, cols, & squares
+        for element_indices in self._generate_board_elements_indices():
+
+            if self._contains_illegal_repeats(
+                self.indicies_to_definite_state(element_indices)
+            ):
                 return False
+
         return True
 
     def isSolved(self):
-        return 0 not in self.state
+        return set(len(i) for i in self.p_state) == {1}
 
-    @staticmethod
-    def _contains_illegal_repeats(board_element: list):
-        c = Counter(board_element)
+    def _contains_illegal_repeats(self, element):
+        c = Counter(element)
         if 0 in c:
-            num_empty = c.pop(0)
+            c.pop(0)
         return len(set(c.values()) - {1}) > 0
 
-    def rows(self, state=None):
-        state = state if state else self.state
+    def rows(self):
+        state = self.p_state
         yield from self._list_chunks(state, 9)
 
-    def row_indices(self, state=None):
-        state = state if state else self.state
+    def row_indices(self):
+        state = self.p_state
         yield from self._list_chunks_indicies(state, 9)
 
-    def cols(self, state=None):
-        state = state if state else self.state
+    def cols(self):
+        state = self.p_state
+        for col_i in self.col_indices(state=state):
+            yield [state[i] for i in col_i]
+
+    def col_indices(self):
+        s = self.p_state
         n = 9  # n columns
         for i in range(n):
-            yield state[i::n]
+            yield range(i, n * n, n)
 
-    def col_indices(self, state=None):
-        state = state if state else self.state
-        n = 9  # n columns
-        for i in range(n):
-            yield range(i, len(state), n)
-
-    def squares(self, state=None):
-        s = state if state else self.state
+    def squares(self):
+        s = self.p_state
         n = 9  # n squares
         three_chunks = list(self._list_chunks(s, 3))
         for i in range(n):
             a = three_chunks[(i % 3) + 9 * (i // 3) :: 3][:3]
             yield [i for j in a for i in j]
 
-    def square_indices(self, state=None):
-        s = state if state else self.state
+    def square_indices(self):
+        s = self.p_state
         n = 9  # n squares
         three_chunks = list(self._list_chunks_indicies(s, 3))
         for i in range(n):
@@ -110,11 +129,6 @@ class SudokuBoard:
         yield from self.cols()
         yield from self.squares()
 
-    def _generate_board_elements_and_pstate(self):
-        yield from zip(self.rows(), self.rows(state=self.p_state))
-        yield from zip(self.cols(), self.cols(state=self.p_state))
-        yield from zip(self.squares(), self.squares(state=self.p_state))
-
     def _generate_board_elements_indices(self):
         yield from self.row_indices()
         yield from self.col_indices()
@@ -122,95 +136,146 @@ class SudokuBoard:
 
     def print_state(self):
         os.system("clear")
-        s = "\n"
-        for i, row in enumerate(self.rows()):
-            for j, el in enumerate(row):
-                p = " " if el == 0 else str(el)
-                s += f"{p} "
-                if j % 3 == 2:
-                    s += " "
+        s = "┣━━━━━╋━━━━━╋━━━━━┫\n"
+        for i, rowi in enumerate(self.row_indices()):
+            s += "┃"
+            for j, el in enumerate(self.p_state[j] for j in rowi):
+                p = " " if len(el) > 1 else str(*el)
+                s += f"{p}"
+
+                s += "┃" if j % 3 == 2 else "│"
             s += "\n"
             if i % 3 == 2:
-                s += "\n"
+                s += "┣━━━━━╋━━━━━╋━━━━━┫\n"
         print(s)
 
     def update_state(self, idx, value):
         self.update_counter += 1
 
-        self.state[idx] = value
         self.p_state[idx] = {value}
         if self.debug:
             assert self.isValid(), "invalid board"
-        self.print_state()
 
-        if self.isSolved():
-            print(f"Solved.")
+        if self.show_state:
+            self.print_state()
+
+    def _find_one_missing(self, element_indices) -> tuple:
+        """
+        check if there's only one missing in row/col/square
+        element_indices: generator of indices for a row, column, or square
+        """
+        row = list(self.indicies_to_definite_state(element_indices))
+        if row.count(0) == 1:
+            v = VALUES - set(row)
+            state_idx = element_indices[row.index(0)]
+            value = v.pop()
+            return state_idx, value
+        else:
+            return 0, 0
+
+    def _eliminate_possibilities(self, element_indices) -> tuple:
+        """
+        check if row/col/square can eliminate possibilities
+        element_indices: generator of indices for a row, column, or square
+        """
+        vals = set(self.indicies_to_definite_state(element_indices))
+        for i in element_indices:
+            possibilities = self.p_state[i]
+
+            if len(possibilities) == 1:
+                continue
+
+            possibilities -= vals  # this line removes the eliminated possibilites
+            if len(possibilities) == 1:
+                return i, possibilities.pop()
+
+            assert len(possibilities) > 0, "something went wrong"
+
+        return 0, 0
+
+    def _check_for_one_place(self, element_indices) -> tuple:
+        """
+        element_indices: generator of indices for a row, column, or square
+        """
+        ps = filter(self.indefinite, (self.p_state[i] for i in element_indices))
+        c = Counter(j for i in ps for j in i)
+        if 1 in c.values():
+            value = {v: k for k, v in c.items()}[1]
+            for i in element_indices:
+                if value in self.p_state[i]:
+                    return i, value
+        return 0, 0
 
     def reduce_possible_state(self):
-        count = 0
-        for row_i in self._generate_board_elements_indices():
-            row = [self.state[i] for i in row_i]
-            c = Counter(row)
-            # check if there's only one missing in row/col/square
-            if c.get(0) == 1:
-                v = set(range(1, 10)) - set(row)
-                idx = row.index(0)
-                state_idx = row_i[idx]
-                value = v.pop()
-                try:
-                    self.update_state(state_idx, value)
-                except:
-                    raise RuntimeError
-                self.reduce_possible_state()
+        if self.isSolved():
+            return 0, 0
 
-        for row_i in self._generate_board_elements_indices():
-            row = [self.state[i] for i in row_i]
-            # check if row/col/square can eliminate possibilities
-            vals = set(row)
-            for i in row_i:
-                if self.state[i] != 0:
-                    continue
+        for element_indices in self._generate_board_elements_indices():
+            for func in [
+                self._find_one_missing,
+                self._eliminate_possibilities,
+                self._check_for_one_place,
+            ]:
+                idx, value = func(element_indices)
+                if value != 0:
+                    return idx, value
 
-                possibilities = self.p_state[i] - vals
-                if len(possibilities) == 1:
-                    self.update_state(i, possibilities.pop())
-                    self.reduce_possible_state()
+        return 0, 0
 
-                elif len(possibilities) == 0:
-                    raise RuntimeError
-
-        # now do a check for if there's only one place
-        # a certain value exists as a possibility
-        for row_i in self._generate_board_elements_indices():
-            ps = (self.p_state[i] for i in row_i)
-            c = Counter(j for i in ps for j in i)
-            if 1 in c.values():
-                ...
-
-    def iteratively_solve(self):
-        iterations = 1
-        changes = self.reduce_possible_state()
-        while changes > 0:
-            iterations += 1
-            changes = self.reduce_possible_state()
-            self.print_state()
-        return
-
-    def _update_p_state_from_state(self):
-        for i, v in enumerate(self.state):
+    def _update_p_state_from_state(self, state):
+        for i, v in enumerate(state):
             if v != 0:
                 self.p_state[i] = {v}
 
-    def _update_state_from_p_state(self):
-        count = 0
-        for i, v in enumerate(self.p_state):
-            if len(v) == 1 and self.state[i] == 0:
-                self.state[i] = v[0]
-                count += 1
-                assert self.isValid(), "not a valid board"
-        return count
+    @staticmethod
+    def definite(s: set):
+        return len(s) == 1
+
+    @staticmethod
+    def indefinite(s: set):
+        return len(s) > 1
+
+    def generate_simple_state(self):
+        yield from self.indefinite_to_definite_state(self.p_state)
+
+    def indefinite_to_definite_state(self, state):
+        for p in state:
+            yield p.copy().pop() if len(p) == 1 else 0
+
+    def indicies_to_possibilites(self, indicies):
+        for i in indicies:
+            yield self.p_state[i]
+
+    def indicies_to_definite_state(self, indicies):
+        yield from self.indefinite_to_definite_state(
+            self.indicies_to_possibilites(indicies)
+        )
+
+
+def gen_test_boards():
+    d = os.path.dirname(__file__)
+    fn = os.path.join(d, "easy50.txt")
+    with open(fn, "r") as f:
+        for line in f:
+            yield line
+
+
+def solve_all_test_boards():
+    for i, board_str in enumerate(gen_test_boards()):
+        print(f"Board {i}")
+        S = SudokuBoard(state_str_to_list(board_str), debug=True)
+        S.solve()
+
+
+def state_str_to_list(state_str):
+    return list(map(int, state_str.strip()))
 
 
 if __name__ == "__main__":
-    S = SudokuBoard(debug=True)
+    solve_all_test_boards()
+
+    S = SudokuBoard(debug=False)
+    S.print_state()
+    S.solve()
+
     ...
