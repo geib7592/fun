8843bf27cb20adf6988a6b6ff4bccfbf61300bd5
geib7592 <gpeterson@psiquantum.com>
2022-12-01 11:43:25 -0800

    make faster

--- fun/aoc2022/day1.py
+++ fun/aoc2022/day1.py
@@ -7,6 +7,31 @@ INPUT_FILE_PATH = Path("fun/aoc2022/input_files/day1.txt")
 
 
 def day1_part1():
+    return max(get_sums())
+
+
+def day1_part2():
+    sums = get_sums()
+    result = sum(sorted(sums, reverse=True)[:3])
+    return result
+
+
+def get_sums():
+    with open(INPUT_FILE_PATH, "r") as f:
+        contents = f.read()
+    grouped_contents = contents.split("\n\n")
+    sums = map(sum_string, grouped_contents)
+    return sums
+
+
+def sum_string(s: str):
+    """
+    s like '18117\n13193\n13899\n19853'
+    """
+    return sum(map(int, s.strip().split("\n")))
+
+
+def day1_part1_1():
     with open(INPUT_FILE_PATH, "r") as f:
         group_sum = 0
         max_sum = 0
@@ -20,7 +45,7 @@ def day1_part1():
     return max_sum
 
 
-def day1_part2():
+def day1_part2_1():
     with open(INPUT_FILE_PATH, "r") as f:
         group_sum = 0
         group_sums = []
@@ -48,11 +73,18 @@ def day1_part2_pandas():
 
 
 def timeit_results():
-    for func in [day1_part1, day1_part1_pandas, day1_part2, day1_part2_pandas]:
-        n = 100
+    for func in [
+        day1_part1,
+        day1_part1_1,
+        day1_part1_pandas,
+        day1_part2,
+        day1_part2_1,
+        day1_part2_pandas,
+    ]:
+        n = 1000
         time = timeit(func, number=n) / n
         print(f"{func.__name__}, {time:.3e} s")
 
 
 if __name__ == "__main__":
-    timeit(day1_part2_pandas, number=1000)
+    timeit_results()
