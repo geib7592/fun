fcc13a1e5abda1a109cef1567ee0e9de826a17ad
Gabe <gabriel.peterson@colorado.edu>
2022-12-04 14:34:10 -0800

    make it faster

--- fun/aoc2022/day04.py
+++ fun/aoc2022/day04.py
@@ -1,8 +1,39 @@
+import re
+
 INPUT_FILE_PATH = "fun/aoc2022/input_files/day04_gap.txt"
 
 
+example_data = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
+
+
+def read_file():
+    with open(INPUT_FILE_PATH, "r") as f:
+        return f.read().strip()
+
+
+def solution(data: str, part=1):
+    """faster version that parses the file once at the beginning"""
+    data = list(map(int, re.split("-|,|\n", data)))
+    s = 0
+    for i in range(0, len(data), 4):
+        a1, a2, b1, b2 = data[i : i + 4]
+        if part == 1:
+            if (a1 <= b1 <= b2 <= a2) or (b1 <= a1 <= a2 <= b2):
+                s += 1
+        elif part == 2:
+            if (a1 <= b1 <= a2) or (b1 <= a1 <= b2):
+                s += 1
+    return s
+
+
+def solution_old(lines_generator, part=1):
+    if part == 1:
+        return len(list(filter(is_full_overlap, lines_generator())))
+    elif part == 2:
+        return len(list(filter(is_partial_overlap, lines_generator())))
+
+
 def generate_example_lines():
-    example_data = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
     for line in example_data.split("\n"):
         yield line
 
@@ -13,24 +44,16 @@ def generate_file_lines():
             yield line
 
 
-def solution(lines_generator, part=1):
-    if part == 1:
-        return len(list(filter(is_full_overlap, lines_generator())))
-    elif part == 2:
-        return len(list(filter(is_partial_overlap, lines_generator())))
-
-
 def split_line(single: str):
-    a, b = single.split(",")
-    a1, a2 = map(int, a.split("-"))
-    b1, b2 = map(int, b.split("-"))
-    return a1, a2, b1, b2
-
-
-def is_full_overlap(single: str):
     """
     single is string like 2-3,4-5
     """
+    a1, b, b2 = single.split("-")
+    a2, b1 = b.split(",")
+    return map(int, (a1, a2, b1, b2))
+
+
+def is_full_overlap(single: str):
     if not single:
         return False
     a1, a2, b1, b2 = split_line(single)
@@ -42,9 +65,6 @@ def is_full_overlap(single: str):
 
 
 def is_partial_overlap(single: str):
-    """
-    single is string like 2-3,4-5
-    """
     if not single:
         return False
     a1, a2, b1, b2 = split_line(single)
@@ -56,8 +76,8 @@ def is_partial_overlap(single: str):
 
 
 if __name__ == "__main__":
-    assert solution(generate_example_lines, part=1) == 2
-    print(solution(generate_file_lines, part=1))
+    assert solution(example_data, part=1) == 2
+    print(solution(read_file(), part=1))
 
-    assert solution(generate_example_lines, part=2) == 4
-    print(solution(generate_file_lines, part=2))
+    assert solution(example_data, part=2) == 4
+    print(solution(read_file(), part=2))
