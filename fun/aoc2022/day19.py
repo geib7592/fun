455c58dcafa7dd4b7d57467f551b493a943c4d8d
geib7592 <gpeterson@psiquantum.com>
2022-12-20 09:51:06 -0800

    d19

--- fun/aoc2022/day19.py
+++ fun/aoc2022/day19.py
@@ -1,11 +1,3 @@
-from itertools import product, repeat, cycle
-import re
-from helpers import timeit_results
-import numpy as np
-import scipy.sparse as ss
-from math import sin, cos, pi, atan2
-import sys, os
-
 example_data = """
 1
 2
@@ -16,8 +8,8 @@ example_data = """
 4
 """.strip()
 
-
 INPUT_FILE_PATH = "fun/aoc2022/input_files/day19_gap.txt"
+KEY = 811589153
 
 
 def read_file():
@@ -25,63 +17,43 @@ def read_file():
         return f.read().strip()
 
 
-def solution1(data: str, part=1):
-    init_data = list(map(int, data.split()))
-    {i: init_data[i] for i in range(len(init_data))}
-
-    indices = list(range(len(init_data)))
-    N = len(init_data)
-    for i in range(len(init_data)):
-        moveby = init_data[i]
-        indices_copy = indices.copy()
-        indices[i] = (indices[i] + moveby) % N
-        change_idx = list(range(abs(moveby)))
-        for j in change_idx:
-            # change all indicies between indicies_copy[i] and indicies[i]
-            idx = (indices_copy[i] + sign(moveby) * (j + 1)) % N
-            idx2 = indices_copy.index(idx)
-            indices[idx2] = (indices_copy[idx2] - sign(moveby)) % N
-
-        new_list = [init_data[k] for k in indices]
-
-        print(indices)
-    return
-
-
 def solution(data: str, part=1):
     init_data = list(map(int, data.split()))
-    d = {i: init_data[i] for i in range(len(init_data))}
 
+    Nloops = 1
+    if part == 2:
+        init_data = [KEY * i for i in init_data]
+        Nloops = 10
+
+    # 'indices' is a map from original position to new position.
+    # original position is index, new position is value
     indices = list(range(len(init_data)))
     N = len(init_data)
-    l = init_data.copy()
-    for i in range(len(init_data)):
-        moveby = init_data[i]
-        indices_copy = indices.copy()
-        idx = indices_copy.index(i)
-        v = indices.pop(idx)
-        indices.insert((idx + moveby) % N, v)
-    return
 
+    for n in range(Nloops):
+        for i in range(N):
+            moveby = init_data[i]
+            idx = indices.index(i)
+            v = indices.pop(idx)
+            ins = idx + moveby  # where to insert
+            if ins == 0 and moveby != 0:
+                # not sure why, but in this case, put at end, not beginning
+                indices.append(v)
+            else:
+                indices.insert(ins % (N - 1), v)
 
-def sign(a):
-    if a > 0:
-        return 1
-    else:
-        return -1
+    new_data = [init_data[i] for i in indices]
+    zero_idx = new_data.index(0)
+    s = sum(new_data[(zero_idx + i) % N] for i in [1000, 2000, 3000])
+    return s
 
 
 if __name__ == "__main__":
     assert solution(example_data, part=1) == 3
     print("Part 1: ")
-    print(solution(read_file(), part=1))
+    print(solution(read_file(), part=1))  # 0.1 s
 
-    assert solution(example_data, part=2) == 1514285714288
+    assert solution(example_data, part=2) == 1623178306
     print("Part 2: ")
-    print(solution(read_file(), part=2))
+    print(solution(read_file(), part=2))  # 1.5 s
 
-    # timeit_results(
-    #     lambda: solution(read_file(), part=1), # 30 ms
-    #     lambda: solution(read_file(), part=2), # 0.8 s
-    #     n=5
-    # )
